7821c3de7d74be75a238373a3be3d81b3a52b547
1wsx10 <1wsx10@gmail.com>
2017-07-29 14:25:28 +1000

    added 'special circle' function that can be used to draw shapes based on the coordinate of each pixel. there are examples of: circle, oval, square, parabola

--- drawlib.c
+++ drawlib.c
@@ -136,6 +136,89 @@ void test() {
 	end(data);
 }
 
+typedef struct oval_data {
+	int radius;
+	int centre_offset;
+	float x_mul;
+	float y_mul;
+} OVAL_DATA;
+
+int oval(const void *x_distance, const void *y_distance, const void *data) {
+	OVAL_DATA od = *(OVAL_DATA*)data;
+	int x = od.centre_offset - *(int*)x_distance;
+	int y = od.centre_offset - *(int*)y_distance;
+	int r = od.radius;
+
+	return x*x * od.x_mul + y*y * od.y_mul < r*r;
+}
+
+typedef struct circle_data {
+	int radius;
+	int centre_offset;
+} CIRCLE_DATA;
+
+int circle(const void *x_distance, const void *y_distance, const void *circle_data) {
+	CIRCLE_DATA cd = *(CIRCLE_DATA*)circle_data;
+	int x = cd.centre_offset - *(int*)x_distance;
+	int y = cd.centre_offset - *(int*)y_distance;
+	int r = cd.radius;
+
+	return x*x + y*y < r*r;
+}
+
+typedef struct parabola_data {
+	int lge_flag;/* -1 for less, 0 for equal, 1 for greater */
+	/* y = ax^2 + bx + c */
+	float a;
+	float b;
+	float c;
+	int side_length;
+} PARABOLA_DATA;
+
+int parabola(const void *x_distance, const void *y_distance, const void *parabola_data) {
+	PARABOLA_DATA pd = *(PARABOLA_DATA*)parabola_data;
+	float x = (float)(*(int*)x_distance);
+	int y = pd.side_length - *(int*)y_distance;
+
+	if(pd.lge_flag < 0) {
+		return y < (int)(pd.a * x*x + pd.b * x + pd.c);
+	} else if(pd.lge_flag > 0) {
+		return y > (int)(pd.a * x*x + pd.b * x + pd.c);
+	} else {
+		return y == (int)(pd.a * x*x + pd.b * x + pd.c);
+	}
+}
+
+int square(const void *x, const void *y, const void *nothing) {
+	/*x = x;
+	y = y;
+	nothing = nothing;*/
+	return 1;
+}
+
+void drawSpecialCircle(FBINFO *data, PIXEL *pixel, int side_len, const void *comp_data, int (*comparison)(const void *x, const void *y, const void *data)) {
+	int i, j;
+	PIXEL tempPix;
+	int t_x;
+	int t_y;
+
+	tempPix.colour = pixel->colour;
+	tempPix.x = &t_x;
+	tempPix.y = &t_y;
+
+	/* a*a + b*b = c*c */
+
+	/* pixels in the square contained by the circle */
+	for(j = 0; j < side_len; j++) {
+		for(i = 0; i < side_len; i++) {
+			if(comparison(&i, &j, comp_data)) {
+				*tempPix.x = *pixel->x + i;
+				*tempPix.y = *pixel->y + j;
+				draw(data, &tempPix);
+			}
+		}
+	}
+}
 
 void drawCircle(FBINFO *data, int r, PIXEL *pixel) {
 	int i, j;
@@ -143,21 +226,17 @@ void drawCircle(FBINFO *data, int r, PIXEL *pixel) {
 	PIXEL tempPix;
 	int t_x;
 	int t_y;
-	float randb;
-	float randa;
 
 	tempPix.colour = pixel->colour;
 	tempPix.x = &t_x;
 	tempPix.y = &t_y;
 
-	randa = (rand()%20)+1;
-	randb = (rand()%20)+1;
 	/* a*a + b*b = c*c */
 
 	/* pixels in the square contained by the circle */
 	for(j = -r; j < r; j++) {
 		for(i = -r; i < r; i++) {
-			if(i*i * ((randa/10)+1) + j*j * ((randb/20)+1) < rSqr) {
+			if(i*i + j*j < rSqr) {
 				*tempPix.x = *pixel->x + i;
 				*tempPix.y = *pixel->y + j;
 				draw(data, &tempPix);
@@ -178,16 +257,22 @@ void clear_screen(FBINFO *data) {
 			pixel.x = &i;
 			pixel.y = &j;
 			draw(data, &pixel);
-		}
+	}
 	}
 }
 
 void rotate_about_point(PIXEL *pixel, int px, int py, float angle) {
+	int temp_x;
+	int temp_y;
+
 	*pixel->x -= px;
 	*pixel->y -= py;
+
+	temp_x = *pixel->x;
+	temp_y = *pixel->y;
 	
-	*pixel->x = (int)(*pixel->x * cos(angle) - *pixel->y * sin(angle));
-	*pixel->y = (int)(*pixel->x * sin(angle) + *pixel->y * cos(angle));
+	*pixel->x = (int)(temp_x * cos(angle) - temp_y * sin(angle));
+	*pixel->y = (int)(temp_x * sin(angle) + temp_y * cos(angle));
 
 	*pixel->x += px;
 	*pixel->y += py;
@@ -204,12 +289,14 @@ int main() {
 	int width;
 	int height;
 	int screensaver = 1;
-	int degrees;
 	float radians;
 	int temp_x;
 	int temp_y;
 	PIXEL randPixel;
 	struct timeval time_of_day;
+	CIRCLE_DATA circle_data;
+	OVAL_DATA oval_data;
+	PARABOLA_DATA parabola_data;
 
 	randPixel.colour = &randColour;
 	randPixel.x = &temp_x;
@@ -219,24 +306,51 @@ int main() {
 
 	/*green.g = 255;
 	blue.b = 255;*/
+	randColour.r = rand()%255;
+	randColour.g = rand()%255;
+	randColour.b = rand()%255;
+	randColour.t = rand()%255;
 
 	data = init();
-	/*
-	drawCircle(data, 20, 30, 5, &green);
-	drawCircle(data, 600, 500, 50, &green);
-	drawCircle(data, 650, 420, 60, &blue);
-	drawCircle(data, 500, 25, 50, &green); */
+
+	*randPixel.x = 500;
+	*randPixel.y = 500;
+	circle_data.radius = 25;
+	circle_data.centre_offset = 25;
+	drawSpecialCircle(data, &randPixel, 50, &circle_data, &circle);
+
+	*randPixel.x = 550;
+	*randPixel.y = 500;
+	oval_data.radius = 25;
+	oval_data.centre_offset = 25;
+	oval_data.x_mul = 1;
+	oval_data.y_mul = 1.5;
+	drawSpecialCircle(data, &randPixel, 50, &oval_data, &oval);
+
+	*randPixel.x = 600;
+	*randPixel.y = 500;
+	drawSpecialCircle(data, &randPixel, 50, NULL, &square);
+
+	parabola_data.lge_flag = -1;
+	parabola_data.a = 0.04f;
+	parabola_data.b = -0.6f;
+	parabola_data.c = 15;
+	parabola_data.side_length = 50;
+	*randPixel.x = 650;
+	*randPixel.y = 500;
+	drawSpecialCircle(data, &randPixel, 50, &parabola_data, &parabola);
+
 	while(screensaver && 0) {
 		sleep(0.1);
-		randColour.r = rand()%255;
-		randColour.g = rand()%255;
-		randColour.b = rand()%255;
-		randColour.t = rand()%255;
 
 		gettimeofday(&time_of_day, NULL);
 
+		randColour.r = (int)(sin(PI * ((double)time_of_day.tv_usec)/1000000) * 255);
+		randColour.r = (int)(cos(PI * ((double)time_of_day.tv_usec)/1000000) * 255);
+		randColour.r = (int)(sin(PI * 0.5 + PI * ((double)time_of_day.tv_usec)/1000000) * 255);
+
 		/*degrees = 60;*/
-		radians = sin(((double)time_of_day.tv_usec)/(1000000)) * 2 * PI;
+		radians = sin(PI * ((double)time_of_day.tv_usec)/(1000000)) * 2 * PI;
 		for(height = 0+400; height < 100+400/*rand()%data->vinfo.yres*/; height++) {
 			for(width = 0+400; width < 100+400/*rand()%data->vinfo.xres*/; width++) {
 				temp_x = width;
@@ -248,7 +362,7 @@ int main() {
 		}
 	}
 
-	while(screensaver) {
+	while(screensaver && 0) {
 		sleep(1);
 		/* draw some random circles */
 		for(i = 0; i < rand()%20; i++) {
